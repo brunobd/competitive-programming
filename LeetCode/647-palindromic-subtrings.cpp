ed463579b79104e4dd4a906a448a52b0764c6ddc
Bruno Duarte <bbduarte@live.com>
2023-07-05 11:22:03 -0300

    codeforces added

--- LeetCode/647-palindromic-subtrings.cpp
+++ LeetCode/647-palindromic-subtrings.cpp
@@ -1,10 +1,9 @@
 class Solution
 {
 public:
-    string longestPalindrome(string s)
+    int longestPalindrome(string s)
     {
-        string res;
-        int res_size = 0;
+        int res=0;
         int size = s.size();
         for (int i = 0; i < size; i++)
         {
@@ -12,11 +11,7 @@ public:
             int l = i, r = i;
             while (l >= 0 and r < size and s[l] == s[r])
             {
-                if ((r - l + 1) > res_size)
-                {
-                    res_size = r - l + 1;
-                    res = s.substr(l, res_size);
-                }
+                res++;
                 l -= 1;
                 r += 1;
             }
@@ -25,11 +20,7 @@ public:
             l = i, r = i+1;
             while (l >= 0 and r < size and s[l] == s[r])
             {
-                if ((r - l + 1) > res_size)
-                {
-                    res_size = r - l + 1;
-                    res = s.substr(l, res_size);
-                }
+                res++;
                 l -= 1;
                 r += 1;
             }
